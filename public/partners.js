43fb59125b2bde02782f3e038c45f8239160b992
Kirill Yusov <kyusov@gmail.com>
2020-11-27 16:41:40 +0800

    d

--- public/partners.js
+++ public/partners.js
@@ -35,7 +35,7 @@ function basketAnimation() {
     anime({
       targets: '.basket-bag-big',
       opacity: [0, 1],
-      translateY: ['-100%', '-35%'],
+      translateY: ['-100%', '-100%'],
       easing: 'easeOutQuart',
       delay: 300
     })
@@ -43,7 +43,7 @@ function basketAnimation() {
     anime({
       targets: '.basket-bag-middle',
       opacity: [0, 1],
-      translateY: ['-150%', '-35%'],
+      translateY: ['-150%', '-100%'],
       easing: 'easeOutQuart',
       delay: 200
     })
@@ -51,7 +51,7 @@ function basketAnimation() {
     anime({
       targets: '.basket-bag-top',
       opacity: [0, 1],
-      translateY: ['-150%', '-35%'],
+      translateY: ['-150%', '-100%'],
       easing: 'easeOutQuart',
       delay: 200
     })
